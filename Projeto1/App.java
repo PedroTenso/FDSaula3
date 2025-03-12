c7572587a7a158cd46641f8ccdc4d9c4663a675b
Tenso <pedro.bittencourt002@edu.pucrs.br>
2025-03-11 21:25:38 -0300

    1
    teste

--- Projeto1/App.java
+++ Projeto1/App.java
@@ -9,7 +9,7 @@ public class App {
         Professor p = new Professor(nome,experiencia);
         System.out.println(p.toString());
         System.out.println("Categoria: "+p.classifica());
+        //Teste
         s.close();  
     }
-    //Teste
 }
